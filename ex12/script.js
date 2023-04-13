e89718f4c898718a26b2dca1614786bf69f4be4b
Larissa <126712393+loyanemenezes@users.noreply.github.com>
2023-04-13 19:06:50 -0300

    update

---
+++ ex12/script.js
@@ -0,0 +1,10 @@
+function calcular() {
+    var ini = document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
+    var res = document.querySelector('div#res')
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = `[ERRO] Verifique os dados e tente novamente!`
+    }
+    if 
+}
