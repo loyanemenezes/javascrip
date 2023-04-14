d7633bc8ef8c0b93915910fc9b82ec35e37ce100
Larissa <126712393+loyanemenezes@users.noreply.github.com>
2023-04-14 15:32:05 -0300

    update

--- ex12/script.js
+++ ex12/script.js
@@ -6,21 +6,21 @@ function calcular() {
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         res.innerHTML = `[ERRO] Verifique os dados e tente novamente!`
     } else {
-        res.innerHTML = 'contando: '
+        res.innerHTML = 'Contando: '
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
         if (p <= 0) {
-            alert('Impossível contar, considerando passo como 1')
+            alert('Impossível contar, considerando passo valendo 1!')
             p = 1
         }
         if (i < f) {
-            for(let c = i;c <= f;c += p) {
+            for(let c = i;c <= f; c += p) {
                 res.innerHTML += ` ${c}`
-            }
+            } 
         } else {
             for(let c = i;c >= f;c -= p) {
-            res.innerHTML += ` ${c}`
+                res.innerHTML += ` ${c}`
             }
         }
         res.innerHTML += `\u{1F3C1}`
